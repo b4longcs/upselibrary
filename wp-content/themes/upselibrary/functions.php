c2f476765f59173970cef7c609a4f5740d86b364
Than <jctubo@up.edu.ph>
2025-04-25 11:49:30 +0800

    fixed console error

--- wp-content/themes/upselibrary/functions.php
+++ wp-content/themes/upselibrary/functions.php
@@ -17,17 +17,21 @@ function enqueue_theme_assets() {
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap&family=Roboto:wght@400;700&display=swap', array(), null, 'all');
     wp_enqueue_style('tex-gyre', 'https://fonts.cdnfonts.com/css/tex-gyre-adventor', array(), null, 'all');
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '4.5.0', 'all');
-    wp_enqueue_style('carouselcss', get_template_directory_uri() . '/assets/css/carousel.css', array(), null, 'all');
-    wp_enqueue_style('front-page', get_template_directory_uri() . '/assets/css/front-page.css', array(), null, 'all');
     wp_enqueue_style('custom-header-css', get_template_directory_uri() . '/assets/css/header.css', array(), null, 'all');
     wp_enqueue_style('custom-upselibrary', get_template_directory_uri() . '/assets/css/custom-upselibrary.css', array(), null, 'all');
+
+    if (is_front_page()) {
+        wp_enqueue_style('carouselcss', get_template_directory_uri() . '/assets/css/carousel.css', array(), null, 'all');
+        wp_enqueue_style('front-page', get_template_directory_uri() . '/assets/css/front-page.css', array(), null, 'all');
+        wp_enqueue_script('carouseljs', get_template_directory_uri() . '/assets/js/carousel.js', array(), null, true);
+        wp_enqueue_script('mainjs', get_template_directory_uri() . '/assets/js/main.js', array(), null, true);
+    }
+
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '4.5.0', true);
     wp_enqueue_script('nav-script', get_template_directory_uri() . '/assets/js/navigation.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('carouseljs', get_template_directory_uri() . '/assets/js/carousel.js', array(), null, true);
-    wp_enqueue_script('mainjs', get_template_directory_uri() . '/assets/js/main.js', array(), null, true);
-
 }
+
 add_action('wp_enqueue_scripts', 'enqueue_theme_assets');
 
 // Customizer settings
